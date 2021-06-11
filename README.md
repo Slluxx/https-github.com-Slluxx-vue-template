25729a4e653997c753914f2804d9d00d63ef0d42
Slluxx <slluxxx@googlemail.com>
2021-06-11 13:56:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-9psme1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-9psme1)
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<script>
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+</script>
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
